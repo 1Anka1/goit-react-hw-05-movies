8cde20fa735623982574d5407b6e975c5ad2b4b7
Your Name <97912793+1Anka1@users.noreply.github.com>
2022-10-16 19:20:23 +0200

    Add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,16 @@
-// import React, { Component } from 'react';
-
-// import api from './api/fetchApi';
-
-
-// export default class App extends Component {
-//   state = {
-//     articles: [],
-//     isLoading: false,
-//     error: null,
-//   };
-
-//   async componentDidMount() {
-//     this.setState({ isLoading: true });
-
-//     try {
-//       const articles = await api.fetchArticlesWithQuery("react");
-//       console.log(articles);
-//       this.setState({ articles });
-//     } catch (error) {
-//       this.setState({ error });
-//     } finally {
-//       this.setState({ isLoading: false });
-//     }
-//   }
-
-//   render() {
-//     const { articles, isLoading, error } = this.state;
-
-//     return (
-//       <div>
-//         {error && <p>Whoops, something went wrong: {error.message}</p>}
-//         {isLoading && <ContentLoader/>}
-//         {articles.length > 0 && <ArticleList articles={articles} />}
-//       </div>
-//     );
-//   }
-// }
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
--- src/components/api/fetchApi.jsx
+++
@@ -1,8 +0,0 @@
-// import axios from "axios";
-
-// export const fetchArticlesWithQuery = async searchQuery => {
-//    const res = await axios.get(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
-//   return res.data.hits;
-// };
-
-// export default {fetchArticlesWithQuery};
